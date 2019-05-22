7198340622d6535f5c23f395b88187acb6d47b50
shadow_of_fox <denisochnev@mail.ru>
2019-05-22 10:44:30 +0300

    start

---
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
