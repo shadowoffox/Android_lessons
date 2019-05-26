0d0020e3916e562a572920466a8b2bb1d0054c33
shadowoffox <denisochnev@mail.ru>
2019-05-26 13:50:45 +0300

    lesson2

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,12 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listView = findViewById(R.id.listView);
 
-        Button button3 = findViewById(R.id.button3);
-        button3.setEnabled(false);
+        final String[] days = new String[] {
+                "Пн + градусы + состояние",
+                "Вт+ градусы + состояние",
+                "Ср+ градусы + состояние",
+                "Чт+ градусы + состояние",
+                "Птн+ градусы + состояние",
+                "Сб+ градусы + состояние",
+                "Вс+ градусы + состояние"
+        };
+       //TODO сделать отсчет от "сегодня" и далее до конца недели. сделать/найти ico для показывания в строках в зависимости от погоды.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, days);
 
-        Switch switch1 = findViewById(R.id.switch1);
-        switch1.setBackgroundColor(Color.YELLOW);
+        listView.setAdapter(adapter);
+
     }
 
 }
